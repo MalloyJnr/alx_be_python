b46e71f8f5fc5b6f8a7295c51a9be5a51a9f5fb9
Malloy-Lucio <mltechguides@gmail.com>
2025-09-14 23:35:48 +0000

    basic operations

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+result = number1 + number2 
+difference = number1 - number2
+product = number2 * number1
+
+print('operation of', number1, 'and', number2, 'is', result)
+print('operation of', number1, 'and', number2, 'is', difference)
+print('operation of', number1, 'and', number2, 'is', product) 
