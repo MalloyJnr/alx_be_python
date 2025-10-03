3a8c65a3ec9e9297b92f3ab68f7ca07e6fb26d23
Malloy-Lucio <mltechguides@gmail.com>
2025-10-03 20:22:07 +0000

    now now

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,11 +3,11 @@ num2 = input("Enter the second number: ")
 operation = input("Choose the operation (+, -, *, /): ").lower()
 match operation:
     case "+":
-        print(f"{num1} + {num2} = {float(num1) + float(num2)}")
+        print(f"The results is {float(num1) + float(num2)}")
     case "-":
-        print(f"{num1} - {num2} = {float(num1) - float(num2)}")
+        print(f"The results is {float(num1) - float(num2)}")
     case "*":
-        print(f"{num1} * {num2} = {float(num1) * float(num2)}")
+        print(f"The results is {float(num1) * float(num2)}")
     case "/":
         if float(num2) == 0:
             print("Error: Division by zero is not allowed.")
